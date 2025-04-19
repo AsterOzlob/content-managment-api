95ef8b748854f1051a45e5a96cb81c0e1737f6dc
AsterOzlob <tumoshenko204@mail.ru>
2025-04-19 19:43:45 +0500

    feat: add configuration handling for database and environment variables

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+}
+
+func LoadConfig() (Config, error) {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		return Config{}, err
+	}
+
+	config := Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBSSLMode:  os.Getenv("DB_SSL_MODE"),
+	}
+
+	return config, nil
+}
