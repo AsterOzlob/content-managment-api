731cfba0d3c70f1d7fb5938a94aa1b9b03ba02b6
AsterOzlob <tumoshenko204@mail.ru>
2025-04-22 21:19:55 +0500

    feat: add DTOs for user and role management including responses and inputs

---
+++ internal/dto/role_dto.go
@@ -0,0 +1,7 @@
+package dto
+
+type RoleResponse struct {
+	ID          uint   `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
---
+++ internal/dto/user_dto.go
@@ -0,0 +1,25 @@
+package dto
+
+type UserResponse struct {
+	ID        uint   `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Role      string `json:"role"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type UserRegistrationInput struct {
+	Username string `json:"username" binding:"required,min=3,max=64"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserRoleAssignmentInput struct {
+	RoleName string `json:"role_name" binding:"required"`
+}
