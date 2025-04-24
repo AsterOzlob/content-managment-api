05f1c23842cfe60b127c15540ced50cef531e4a9
AsterOzlob <tumoshenko204@mail.ru>
2025-04-24 18:44:33 +0500

    feat: add Articles field to User model for content association

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,13 +4,15 @@ import "time"
 
 // User представляет пользователя системы.
 type User struct {
-	ID           uint       `json:"id" gorm:"primaryKey"`                    // Уникальный идентификатор пользователя.
-	RoleID       uint       `gorm:"not null;index"`                          // ID роли пользователя
-	Role         Role       `gorm:"foreignKey:RoleID"`                       // Связь с ролью
-	Username     string     `json:"username" gorm:"unique;not null;size:64"` // Уникальное имя пользователя.
-	Email        string     `json:"email" gorm:"unique;size:255"`            // Уникальный email пользователя.
-	PasswordHash string     `json:"-" gorm:"not null"`                       // Хэшированный пароль (скрыт из JSON).
-	CreatedAt    time.Time  `json:"created_at"`                              // Дата создания записи.
-	UpdatedAt    time.Time  `json:"updated_at"`                              // Дата последнего обновления записи.
-	DeletedAt    *time.Time `json:"deleted_at,omitempty" gorm:"index"`       // Дата удаления записи (если применимо).
+	ID            uint           `json:"id" gorm:"primaryKey"`                    // Уникальный идентификатор пользователя.
+	RoleID        uint           `gorm:"not null;index"`                          // ID роли пользователя
+	Role          Role           `gorm:"foreignKey:RoleID"`                       // Связь с ролью
+	Username      string         `json:"username" gorm:"unique;not null;size:64"` // Уникальное имя пользователя.
+	Email         string         `json:"email" gorm:"unique;size:255"`            // Уникальный email пользователя.
+	PasswordHash  string         `json:"-" gorm:"not null"`                       // Хэшированный пароль (скрыт из JSON).
+	CreatedAt     time.Time      `json:"created_at"`                              // Дата создания записи.
+	UpdatedAt     time.Time      `json:"updated_at"`                              // Дата последнего обновления записи.
+	DeletedAt     *time.Time     `json:"deleted_at,omitempty" gorm:"index"`       // Дата удаления записи (если применимо).
+	Articles      []Article      `json:"articles" gorm:"foreignKey:AuthorID"`     // Связь с контентом
+	RefreshTokens []RefreshToken `gorm:"foreignKey:UserID"`                       // Связь с токенами
 }
