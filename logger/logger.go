63e641329e48f6fa3feaddffaa17aa79932cc0e0
AsterOzlob <tumoshenko204@mail.ru>
2025-04-24 17:36:15 +0500

    feat: add customizable logger with logrus
    - Introduced `Logger` type wrapping logrus.Logger.
    - Added `NewLogger` for JSON logging and file output.
    - Ensures directory creation and sets Debug level.

---
+++ logger/logger.go
@@ -0,0 +1,45 @@
+package logging
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Logger представляет настраиваемый логгер.
+type Logger struct {
+	*logrus.Logger
+}
+
+// NewLogger создает новый экземпляр логгера.
+func NewLogger(logFile string) *Logger {
+	logger := logrus.New()
+
+	// Настройка формата логов
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	// Получаем директорию из пути к файлу
+	dir := filepath.Dir(logFile)
+
+	// Проверяем, существует ли директория, и создаем её, если её нет
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			logger.Fatalf("Failed to create log directory: %v", err)
+		}
+	}
+
+	// Создание файла для логов
+	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		logger.Fatalf("Failed to open log file: %v", err)
+	}
+
+	// Направление вывода логов в файл
+	logger.SetOutput(file)
+
+	// Уровень логирования
+	logger.SetLevel(logrus.DebugLevel)
+
+	return &Logger{logger}
+}
