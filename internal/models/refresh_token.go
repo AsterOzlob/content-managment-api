76527208f0a3af2e25ec052f7581d1464f258a47
AsterOzlob <tumoshenko204@mail.ru>
2025-04-30 20:19:17 +0500

    feat(auth): added RefreshToken model and repository

--- internal/models/refresh_token.go
+++ internal/models/refresh_token.go
@@ -4,13 +4,13 @@ import "time"
 
 // RefreshToken представляет refresh token для аутентификации.
 type RefreshToken struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`                            // Уникальный идентификатор токена.
-	UserID    uint      `json:"user_id" gorm:"not null;index"`                   // Идентификатор пользователя, которому принадлежит токен.
-	Token     string    `json:"token" gorm:"unique;not null;size:512"`           // Сам токен.
-	UserAgent string    `json:"user_agent" gorm:"size:255"`                      // User-Agent клиента.
-	IP        string    `json:"ip" gorm:"size:45"`                               // IP-адрес клиента.
-	ExpiresAt uint64    `json:"expires_at" gorm:"not null;type:Timestamp;index"` // Время истечения токена.
-	Revoked   bool      `json:"revoked" gorm:"default:false"`                    // Отозван ли токен.
-	CreatedAt time.Time `json:"created_at"`                                      // Дата создания токена.
-	UpdatedAt time.Time `json:"updated_at"`                                      // Дата последнего обновления записи.
+	ID        uint      `json:"id" gorm:"primaryKey"`                  // Уникальный идентификатор токена.
+	UserID    uint      `json:"user_id" gorm:"not null;index"`         // Идентификатор пользователя, которому принадлежит токен.
+	Token     string    `json:"token" gorm:"unique;not null;size:512"` // Сам токен.
+	UserAgent string    `json:"user_agent" gorm:"size:255"`            // User-Agent клиента.
+	IP        string    `json:"ip" gorm:"size:45"`                     // IP-адрес клиента.
+	ExpiresAt time.Time `json:"expires_at" gorm:"not null;index"`      // Время истечения токена.
+	Revoked   bool      `json:"revoked" gorm:"default:false;index"`    // Отозван ли токен.
+	CreatedAt time.Time `json:"created_at"`                            // Дата создания токена.
+	UpdatedAt time.Time `json:"updated_at"`                            // Дата последнего обновления записи.
 }
